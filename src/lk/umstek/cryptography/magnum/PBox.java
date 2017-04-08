b8b03054894aa55045be141b8900178ee887c79c
Wickramaranga Abeygunawardhana <umstek@live.com>
2017-04-08 17:48:56 +0530

    Correct permutation operation reversal error

--- src/lk/umstek/cryptography/magnum/PBox.java
+++ src/lk/umstek/cryptography/magnum/PBox.java
@@ -25,7 +25,7 @@ public class PBox {
                 if (encrypt) { // lookup
                     lookup[i][j] = (PRIMES[key[i * 2] + 128] * j + (key[i * 2 + 1] + 128)) % 4;
                 } else { // reverse-lookup
-                    lookup[i][(PRIMES[key[i * 2] + 128] * j + (key[i * 2 + 1] + 128)) % 4] = j;
+                    lookup[8 - i - 1][(PRIMES[key[i * 2] + 128] * j + (key[i * 2 + 1] + 128)) % 4] = j;
                 }
             }
         }
@@ -34,15 +34,15 @@ public class PBox {
         byte[] out = Arrays.copyOf(block, 16);
         for (int i = 0; i < 8; i++) {
             byte[] temp = new byte[16];
-            if (i % 2 == 0) { // row
+            if (encrypt ^ i % 2 != 0) { // row (since decryption is the reverse operation, odd/even are swapped)
                 for (int r = 0; r < 4; r++) {
                     for (int c = 0; c < 4; c++) {
                         temp[r * 4 + c] = out[lookup[i][r] * 4 + c];
                     }
                 }
             } else { // column
-                for (int r = 0; r < 4; r++) {
-                    for (int c = 0; c < 4; c++) {
+                for (int c = 0; c < 4; c++) {
+                    for (int r = 0; r < 4; r++) {
                         temp[r * 4 + c] = out[r * 4 + lookup[i][c]];
                     }
                 }
